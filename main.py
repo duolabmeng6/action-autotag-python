38a5b75eb87bce1c6e5e5f44937ce1e9ff473ee8
多啦b梦 <1715109585@qq.com>
2022-07-14 13:18:09 +0800

    ok

--- main.py
+++ main.py
@@ -1,29 +1,20 @@
 import os
 from pathlib import Path
-
 from github import Github
 
 
 def main():
-    about = {}
     with Path(os.environ["INPUT_PATH"]).open() as f:
-        exec(f.read(), about)
-
-    prefix = os.environ["INPUT_PREFIX"]
-    variable = os.environ["INPUT_VARIABLE"]
-    suffix = os.environ["INPUT_SUFFIX"]
-    version_tag = f"{prefix}{about[variable]}{suffix}"
-
+        v = f.read()
+    version_tag = f"{v}"
+    os.environ['version'] = version_tag
     g = Github(os.environ["INPUT_GITHUB_TOKEN"])
     repo = g.get_repo(os.environ["github"]["repository"])
-
     for tag in repo.get_tags():
         if tag.name == version_tag:
             return
-
     sha = repo.get_commits()[0].sha
     repo.create_git_ref(f"refs/tags/{version_tag}", sha)
-
 
 if __name__ == "__main__":
     main()
