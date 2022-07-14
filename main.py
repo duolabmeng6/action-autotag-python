f43352e67080bd5d327b6b7e6f25312c2c379de8
多啦b梦 <1715109585@qq.com>
2022-07-14 15:13:09 +0800

    ok

--- main.py
+++ main.py
@@ -3,22 +3,11 @@ from pathlib import Path
 from github import Github
 
 def main():
-    my_input = os.environ["INPUT_MYINPUT"]
-    my_output = f"Hello {my_input}"
+    Name = os.environ["INPUT_GITHUB_ACTION_REPOSITORY"]
+    my_output = f"Hello {Name}"
     variable = "1.0.0"
-
     print(f"::set-output name=myOutput::{my_output}")
     print(f"::set-output name=version::{variable}")
-
-    g = Github(os.environ["INPUT_TOKEN"])
-    repo = g.get_repo(os.environ["github"]["repository"])
-    for tag in repo.get_tags():
-        if tag.name == variable:
-            return
-    sha = repo.get_commits()[0].sha
-    repo.create_git_ref(f"refs/tags/{variable}", sha)
-
-
 
 if __name__ == "__main__":
     main()
