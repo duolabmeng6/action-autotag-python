c870a86e5e5e99b9d535532f21fb2d483a5b4fe2
多啦b梦 <1715109585@qq.com>
2022-07-14 13:49:01 +0800

    ok

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ import requests  # noqa We are just importing this to prove the dependency insta
 
 def main():
     my_input = os.environ["INPUT_MYINPUT"]
-
     my_output = f"Hello {my_input}"
+    variable = "1.0.0"
 
     print(f"::set-output name=myOutput::{my_output}")
+    print(f"::set-output name=version::{variable}")
+
+    g = Github(os.environ["INPUT_TOKEN"])
+    repo = g.get_repo(os.environ["github"]["repository"])
+    for tag in repo.get_tags():
+        if tag.name == variable:
+            return
+    sha = repo.get_commits()[0].sha
+    repo.create_git_ref(f"refs/tags/{variable}", sha)
+
 
 
 if __name__ == "__main__":
