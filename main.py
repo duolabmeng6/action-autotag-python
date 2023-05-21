391d8df2be4acb6e1b2a163e3941c1c3b8a2ffde
多啦b梦 <1715109585@qq.com>
2023-05-21 11:57:56 +0800

    ok

--- main.py
+++ main.py
@@ -3,51 +3,35 @@ import os
 
 from github import Github
 
+def 版本号递进(version_str):
+    # 版本号格式为 x.x.x 满十进一
+    version = version_str.split('.')
+    if len(version) == 2:
+        version.append('0')
 
-def 版本号格式加一(版本号):
-    版本号 = 版本号.split('.')
-    版本号[-1] = str(int(版本号[-1]) + 1)
-    版本号 = '.'.join(版本号)
-    return 版本号
+    version = list(map(int, version))
 
+    version[2] += 1
+    if version[2] >= 10:
+        version[2] = 0
+        version[1] += 1
+        if version[1] >= 10:
+            version[1] = 0
+            version[0] += 1
 
-# print("版本号格式加一:", 版本号格式加一("1.0.0"))
-# def 版本号大小比较(版本号1, 版本号2):
-#     版本号1 = 版本号1.split('.')
-#     版本号2 = 版本号2.split('.')
-#     for i in range(len(版本号1)):
-#         if int(版本号1[i]) > int(版本号2[i]):
-#             return True
-#         elif int(版本号1[i]) < int(版本号2[i]):
-#             return False
-#     return False
-#
-#
-# print("版本号大小比较:", 版本号大小比较("1.0.0", "1.0.1"))
-# print("版本号大小比较:", 版本号大小比较("1.0.0", "2.0.1"))
-# print("版本号大小比较:", 版本号大小比较("3.0.0", "2.0.1"))
-
+    return '.'.join(map(str, version))
 
 def 版本号从大小写排序(tags):
-    # 删除非数字的版本号
-    tags = [tag for tag in tags if tag.replace('.', '').isdigit()]
-    tags_dict = []
-    for tag in tags:
-        # 获取数值
-        tag_value = int("".join(tag.split('.')))
-        tags_dict.append({
-            "tag": tag,
-            'tagint': tag_value
-        })
-    tags_dict.sort(key=lambda student: student['tagint'])
-    tags_dict.reverse()
-    # 重新组装
-    tags = []
-    for tag in tags_dict:
-        tags.append(tag['tag'])
-    return tags
-
-# print("版本号从大小写排序:", 版本号从大小写排序(['0.0.10', '0.0.9', '3.2.8', '0.1.7', "v1.0", "latest"]))
+    # 将版本号字符串转换为列表
+    arr = [v.split('.') for v in tags]
+    # 将版本号列表转换为数字列表
+    arr = [[int(n) for n in v] for v in arr]
+    # 使用 Python 内置排序函数，按照数字列表的顺序进行排序
+    arr = sorted(arr)
+    # 将数字列表转换回版本号列表
+    arr = ['.'.join([str(n) for n in v]) for v in arr]
+    arr.reverse()
+    return arr
 
 
 
@@ -87,7 +71,7 @@ def 检查当前项目并且将版本号码加一(token, project_name):
     # 版本号排序
     tags = 版本号从大小写排序(tags)
     # print("版本号排序:", tags)
-    新版本号 = 版本号格式加一(tags[0])
+    新版本号 = 版本号递进(tags[0])
     # print("新版本号:", 新版本号)
     print("创建新版本", 新版本号)
     sha = repo.get_commits()[0].sha
